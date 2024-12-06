0b0787418cfdb079b5b91412af14a8744b697859
Elsad Fazllija <e.fazllija@kuramasrl.com>
2024-12-06 12:47:45 +0100

    added provinces

--- src/layouts/main-layout/menus/UserAccountMenu.vue
+++ src/layouts/main-layout/menus/UserAccountMenu.vue
@@ -57,7 +57,7 @@
 
     <!--begin::Menu item-->
     <!-- <div class="menu-item px-5 my-1">
-      <router-link to="/pages/profile/overview" class="menu-link px-5">
+      <router-link :to="{ name: 'profile_details', params: { id: user.Id }}" class="menu-link px-5">
         Account Settings
       </router-link>
     </div> -->
