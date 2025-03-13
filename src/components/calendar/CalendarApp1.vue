5128e947c969cb17532c9e30b6debe4a900d78e8
Alsajet <a.molla@gruppointent.com>
2025-03-13 10:03:53 +0100

    search  calendar

--- src/components/calendar/CalendarApp1.vue
+++ src/components/calendar/CalendarApp1.vue
@@ -5,17 +5,26 @@
     <!--begin::Card header-->
     <div class="card-header">
       <h2 class="card-title fw-bold">Calendar</h2>
-      <div v-if="user.Role == 'Admin'" class="card-toolbar col-lg-4 col-md-9 col-sm-12">
+      <div v-if="user.Role == 'Admin'" class="card-toolbar col-lg-3 col-md-9 col-sm-12">
         <select class="form-control selectpicker" v-model="agencyId">
           <option v-for="(item, index) in searchItems.Agencies" :key="index" :value="item.Id">{{ item.Name }}</option>
         </select>
       </div>
-      <div v-if="user.Role == 'Admin' || user.Role == 'Agency'" class="card-toolbar col-lg-4 col-md-9 col-sm-12">
+      <div v-if="user.Role == 'Admin' || user.Role == 'Agency'" class="card-toolbar col-lg-3 col-md-9 col-sm-12">
         <select class="form-control selectpicker" v-model="agentId">
           <option value>Visualizza tutti gli agenti</option>
           <option v-for="(item, index) in searchItems.Agents" :key="index" :value="item.Id">{{ item.Name }} {{
             item.LastName }}</option>
         </select>
+      </div>
+      <div class="card-toolbar col-lg-3 col-md-9 col-sm-12">
+        <input
+      type="text"
+      class="form-control"
+      v-model="search"
+      @input="searchItemsFunc()"
+      placeholder="Cerca evento, descrizione..."
+    />
       </div>
       <div class="card-toolbar">
         <button class="btn btn-flex btn-primary" @click="newEvent(null, null)">
@@ -51,7 +60,7 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import listPlugin from "@fullcalendar/list";
 import interactionPlugin from "@fullcalendar/interaction";
-import events, { TODAY, todayDate, getEvents, getSearchItems, Event, SearchModel } from "@/core/data/events";
+import events, { TODAY, todayDate, getEvents, getSearchItems, Event, SearchModel, getToInsert } from "@/core/data/events";
 import NewEventModal from "@/components/modals/forms/calendar/NewEventModal.vue";
 import UpdateEventModal from "@/components/modals/forms/calendar/UpdateEventModal.vue";
 import { Modal } from "bootstrap";
@@ -60,6 +69,9 @@ import type { EventInput } from "@fullcalendar/core";
 import Loading from "@/components/kt-datatable/table-partials/Loading.vue";
 import itLocale from '@fullcalendar/core/locales/it';
 import { end, start } from "@popperjs/core";
+import Multiselect from '@vueform/multiselect';
+import { MenuComponent } from "@/assets/ts/components";
+
 
 export default defineComponent({
   name: "calendar-app-1",
@@ -67,7 +79,8 @@ export default defineComponent({
     FullCalendar,
     NewEventModal,
     UpdateEventModal,
-    Loading
+    Loading,
+    Multiselect
   },
   setup() {
     const loading = ref<boolean>(true);
@@ -76,6 +89,7 @@ export default defineComponent({
     let selectedDateEnd = ref<string>("");
     let agentId = ref("");
     let agencyId = ref("");
+    const initItems = ref([]);
     const newTargetModalRef = ref<null | HTMLElement>(null);
     const store = useAuthStore();
     const user = store.user;
@@ -141,48 +155,49 @@ export default defineComponent({
     const tableData = ref<Array<EventInput>>([]);
 
     async function getItems(filterRequest: string) {
-    
       tableData.value.splice(0);
+      initItems.value.splice(0);
       const results = await getEvents(filterRequest);
       const addName = store.user.Role != "Agent" && agentId.value == "" ? true : false;
       for (const key in results) {
 
-        const item = {
+    const item = {
           id: results[key].Id.toString(),
           title: addName ? `${results[key].ApplicationUser.Name} ${results[key].ApplicationUser.LastName}: ${results[key].NomeEvento}` : results[key].NomeEvento,
           start: results[key].DataInizioEvento,
           end: results[key].DataFineEvento,
           description: results[key].DescrizioneEvento,
-          className: "fc-event-meeting",
+      className: "fc-event-meeting",
           color: results[key].Color != null && results[key].Color != "" ? results[key].Color : results[key].ApplicationUser.Color
-        } as EventInput;
+    } as EventInput;
 
         tableData.value.push(item)
-      }
+  }
+     initItems.value.splice(0, tableData.value.length, ...tableData.value);
 
       if (store.user.Role == "Agency" || store.user.Role == "Admin") {
         defaultSearchItems.value = await getSearchItems(store.user.Id);
         searchItems.value.Agencies = defaultSearchItems.value.Agencies;
         if (store.user.Role == "Agency") {
-          agencyId.value = store.user.Id;
+      agencyId.value = store.user.Id;
           searchItems.value.Agents = defaultSearchItems.value.Agents.filter(x => x.AgencyId == agencyId.value);
-        } else {
-          agencyId.value = defaultSearchItems.value.Agencies[0].Id;
-          searchItems.value.Agents = defaultSearchItems.value.Agents;
-        }
-      }
+    } else {
+      agencyId.value = defaultSearchItems.value.Agencies[0].Id;
+      searchItems.value.Agents = defaultSearchItems.value.Agents;
+  }
+}
 
      
     };
 
     const searchItems = ref<SearchModel>({
       Agencies: [],
-      Agents: []
+      Agents: [],
     })
 
     const defaultSearchItems = ref<SearchModel>({
       Agencies: [],
-      Agents: []
+      Agents: [],
     })
 
     onMounted(async () => {
@@ -206,6 +221,37 @@ export default defineComponent({
         await getItems("");
       }
     })
+   
+
+    const search = ref<string>("");
+    const searchItemsFunc = () => {
+      tableData.value.splice(0, tableData.value.length, ...initItems.value);
+      if (search.value !== "") {
+        let results: Array<EventInput> = [];
+        for (let j = 0; j < tableData.value.length; j++) {
+          if (searchingFunc(tableData.value[j], search.value)) {
+            results.push(tableData.value[j]);
+          }
+        }
+        tableData.value.splice(0, tableData.value.length, ...results);
+      }
+      MenuComponent.reinitialization();
+    };
+
+    const searchingFunc = (obj: any, value: string): boolean => {
+      for (let key in obj) {
+        if (
+          !Number.isInteger(obj[key]) &&
+          !(typeof obj[key] === "object") &&
+          (typeof obj[key] === "string" || typeof obj[key] === "number" || Array.isArray(obj[key]))
+        ) {
+          if (obj[key].toString().toLowerCase().indexOf(value) !== -1) {
+            return true;
+          }
+        }
+      }
+      return false;
+    };
 
     const calendarOptions = {
       plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin],
@@ -276,7 +322,9 @@ export default defineComponent({
       todayDate,
       newTargetModalRef,
       userId,
-      color
+      color,
+      search,
+      searchItemsFunc
     };
   },
 });
